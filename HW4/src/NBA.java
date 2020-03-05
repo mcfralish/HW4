987e7b7879028ee5a5551c6c4fa62f644cfbfe7b
mcfralish <mcfralish@gmail.com>
2020-03-04 23:25:05 -0500

    finished NBA

--- HW4/src/NBA.java
+++ HW4/src/NBA.java
@@ -1,8 +1,67 @@
+import java.util.*;
 
 public class NBA {
+	
+	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		
+
+
+		
+		// Construct Team Heat
+		System.out.println("Creating the NBA Team \"Heat\".");
+		NBATeam heat = new NBATeam("Heat");
+		addPlayers(heat);
+		
+		
+		// Construct Team Spurs
+		System.out.println("Creating the NBA Team \"Spurs\".");
+		NBATeam spurs = new NBATeam("Spurs");
+		addPlayers(spurs);
+		
+		// Simulates Games
+		System.out.println("Game on Now...");
+		System.out.println();
+		while(heat.getWins()<7&&heat.getLosses()<7) {
+			double win = Math.random();
+			if (win>.5) heat.winGame(spurs);
+			else spurs.winGame(heat);
+		}
+		
+		// Prints Results
+		if (heat.getWins()==7) System.out.println("Heat win the series!\n");
+		else System.out.println("Spurs win the series!\n");
+		System.out.println(heat);
+		System.out.println();
+		System.out.println(spurs);
+		
+	}
+	
+	public static void addPlayers(NBATeam name) {
+		String ifAddPlayer;
+		String playerName;
+		System.out.println("Add a player to the"+ name.getTeamName()+ "? (yes/no)");
+		ifAddPlayer = sc.next();
+		while(!ifAddPlayer.equalsIgnoreCase("yes")&&!ifAddPlayer.equalsIgnoreCase("no")) {
+			System.out.println("Invalid Entry.");
+			System.out.println("Add a player to the \"Heat\"? (yes/no");
+			ifAddPlayer = sc.next();
+		}
+		
+		while (ifAddPlayer.equalsIgnoreCase("yes")) {
+			System.out.println("What is the name to be added?");
+			playerName = sc.next();
+			name.addAPlayer(playerName);
+			
+			System.out.println("Add another player to the"+ name.getTeamName()+"? (yes/no)");
+			ifAddPlayer = sc.next();
+			while(!ifAddPlayer.equalsIgnoreCase("yes")&&!ifAddPlayer.equalsIgnoreCase("no")) {
+				System.out.println("Invalid Entry.");
+				System.out.println("Add a player to the \"Heat\"? (yes/no");
+				ifAddPlayer = sc.next();
+			}
+		}
 	}
 
 }
--- HW4/src/NBATeam.java
+++ HW4/src/NBATeam.java
@@ -1,4 +1,53 @@
-
+import java.util.*;
 public class NBATeam {
+	
+	private String sTeamName;
+	private int nWin;
+	private int nLoss;
+	private int numPlayers = 0;
+	
+	private String[] playerArray;
+	
+	public NBATeam(String name) {
+		sTeamName = name;
+		nWin = 0;
+		nLoss = 0;
+		playerArray = new String[0];
+	}
+	
+	public String toString() {
+		return sTeamName+"\n"+Arrays.toString(playerArray)+"\nWins: "+nWin+"\nLosses: "+nLoss;
+	}
+	
+	public void addAPlayer(String name) {
+		numPlayers++;
+		String[] tempPlayers = new String[numPlayers];
+		for (int i=0;i<playerArray.length;i++) {
+			tempPlayers[i]=playerArray[i];
+		}
+		tempPlayers[numPlayers-1]=name;
+		playerArray=tempPlayers;
+	}
+	
+	public void winGame(NBATeam opponent) {
+		nWin++;
+		opponent.loseGame();
+	}
+	
+	public String getTeamName() {
+		return sTeamName;
+	}
+	
+	public void loseGame() {
+		nLoss++;
+	}
+	
+	public int getWins() {
+		return nWin;
+	}
+	
+	public int getLosses() {
+		return nLoss;
+	}
 
 }
